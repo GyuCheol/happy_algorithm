c57c72df0de944c7424c43b877f0c72cb2685dc8
delphi9 <grandelphi9@gmail.com>
2020-04-11 20:16:47 -0700

    4.11

---
+++ 200409_python_study3/오픈채팅방.py
@@ -0,0 +1,27 @@
+# '[닉네임] 님이 들어왔습니다
+# '[닉네임] 님이 나갔습니다
+
+새로운 닉입장 or 닉변경
+
+닉변경시 기존 알림창 닉도 같이 변경 -> 포인터나 shallowcopy??
+메모리 주소 받아서 가져오는 것..
+
+record가 주어질 때, 최종적으로 방을 개설한 사람이 보게 되는 메세지
+하나씩 해결해보자.
+
+d = {}
+def solution():
+    nick,action = input().split(' ')
+    d[nick] = action
+
+    if action == 'leave':
+        d.del[nick]
+    elif action == 'change':
+        d[nick] = nick.copy()
+    else:
+
+
+    
+print('\n', join(name))
+
+
