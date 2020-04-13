5b919b572fe91717c3cac82a86a7f5e5f390cc54
delphi9 <grandelphi9@gmail.com>
2020-04-12 21:27:19 -0700

    4.12

--- 200409_python_study3/오픈채팅방.py
+++ 200409_python_study3/오픈채팅방.py
@@ -1,27 +1,35 @@
 # '[닉네임] 님이 들어왔습니다
 # '[닉네임] 님이 나갔습니다
 
-새로운 닉입장 or 닉변경
 
-닉변경시 기존 알림창 닉도 같이 변경 -> 포인터나 shallowcopy??
-메모리 주소 받아서 가져오는 것..
+tmp = {
+    'Enter' : '들어왔습니다.',
+    'Leave': '나갔습니다.'
+}
 
-record가 주어질 때, 최종적으로 방을 개설한 사람이 보게 되는 메세지
-하나씩 해결해보자.
+def solution(records):
+    log = []
+    d = {}
 
-d = {}
-def solution():
-    nick,action = input().split(' ')
-    d[nick] = action
+    for r in records:
+        cmds = r.split(' ')
+        action = cmds[0]
+        id = cmds[1]
 
-    if action == 'leave':
-        d.del[nick]
-    elif action == 'change':
-        d[nick] = nick.copy()
-    else:
+        if action[0] == 'E':
+            log.append((id, action))
+            d[id] = cmds[2]
+        elif action[0] == 'L':
+            log.append((id, action))
+        else:
+            d[id] = cmds[2]
+            
+    return [f'{d[id]}님이 {tmp[action]}' for id, action in log]
 
+# formatting string
+# a, b = 'A', 'C'
+# f'{a}님이 {b}합니다.'
+# '%s님이 %s합니다.' % (a, b)
 
-    
-print('\n', join(name))
-
+print(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]))
 
