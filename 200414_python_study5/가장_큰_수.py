08b043bd3fa81093e9acf968cd814e80a8c23348
rbcjf0219 <rbcjf0219@gmail.com>
2020-04-14 09:58:05 -0700

    f3

--- 200414_python_study5/가장_큰_수.py
+++ 200414_python_study5/가장_큰_수.py
@@ -1,7 +1,26 @@
+import functools
+
+def str_comp(x: str, y: str):
+
+    x_len, y_len = len(x), len(y)
+
+    for i in range(4):
+        x_id = i % x_len
+        y_id = i % y_len
+
+        if x[x_id] == y[y_id]:
+            continue
+        elif x[x_id] > y[y_id]:
+            return 1
+        else:
+            return -1
+
+
+    return 0
 
 def solution(numbers):
     str_list = list(map(str, numbers))
-    str_list = sorted(str_list, key=lambda s: s * 3, reverse=True)
+    str_list = sorted(str_list, key=functools.cmp_to_key(str_comp), reverse=True)
 
     return str(int(''.join(str_list)))
 
