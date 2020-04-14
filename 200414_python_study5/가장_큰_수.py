937c42bb00646c30897b9cc238fa674ea6df42b6
rbcjf0219 <rbcjf0219@gmail.com>
2020-04-14 10:06:30 -0700

    o3o

--- 200414_python_study5/가장_큰_수.py
+++ 200414_python_study5/가장_큰_수.py
@@ -1,10 +1,9 @@
 import functools
 
-def str_comp(x: str, y: str):
-
+def str_comp(max_len: int, x: str, y: str):
     x_len, y_len = len(x), len(y)
-
-    for i in range(4):
+    
+    for i in range(max_len):
         x_id = i % x_len
         y_id = i % y_len
 
@@ -20,8 +19,10 @@ def str_comp(x: str, y: str):
 
 def solution(numbers):
     str_list = list(map(str, numbers))
-    str_list = sorted(str_list, key=functools.cmp_to_key(str_comp), reverse=True)
+    max_len = max([len(s) for s in str_list])
+    str_list = sorted(str_list, key=functools.cmp_to_key(lambda x, y: str_comp(max_len, x, y)), reverse=True)
 
     return str(int(''.join(str_list)))
 
 print(solution([121, 12]))
+print(solution([6, 10, 2]))
