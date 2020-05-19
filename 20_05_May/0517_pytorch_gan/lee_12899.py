00d9178dbb16abfb8b272febfb36f7e58a977b7a
rbcjf0219 <rbcjf0219@gmail.com>
2020-05-18 18:06:39 -0700

    5/18 일부 소스 수정

--- 20_05_May/0517_pytorch_gan/lee_12899.py
+++ 20_05_May/0517_pytorch_gan/lee_12899.py
@@ -1,18 +1,25 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12899
 
+def reverse(l):
+
+    for n in range(len(l) - 1, -1, -1):
+        yield l[n]
 
 def solution(n):
-    answer = ''
+    answer = []
 
     while n > 0:
         tmp = n % 3
         n //= 3
 
+        # 3진법이지만, 0을 자릿수로 쓰지 않음
+        # 그러므로 3으로 나누어 떨어지는 구간에 4를 표시
         if tmp == 0:
             n -= 1
-            answer += '4'
+            answer.append('4')
         else:
-            answer += str(tmp)
+            answer.append(str(tmp))
 
-    return answer[::-1]
+    return ''.join(reverse(answer))
 
 print(solution(10))
