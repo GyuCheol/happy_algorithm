02be8f94fbd54315826340354b051dc0e1dcd32e
delphi9 <grandelphi9@gmail.com>
2020-06-14 14:05:36 -0700

    clean push

---
+++ 20_06_June/0611_algorithm/jiwon_쇠막대기.py
@@ -0,0 +1,11 @@
+def solution(arrangement):
+    stack = []
+    for i in len(arrangement):
+        if arrangement[i] == '(':
+            add.stack(arrangement[i])
+        elif arrangement[i] == ')':
+
+            
+
+
+    return answer
