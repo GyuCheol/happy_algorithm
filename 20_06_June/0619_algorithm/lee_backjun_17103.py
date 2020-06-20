fd6a8b78b37808fe6ab3aafd80aa8bfef01bcc9a
GyuCheol <rbcjf0219@gmail.com>
2020-06-20 12:57:04 -0700

    6/20 수학 연습 마무리

--- 20_06_June/0619_algorithm/lee_backjun_17103.py
+++ 20_06_June/0619_algorithm/lee_backjun_17103.py
@@ -24,34 +24,46 @@ def is_prime(n):
     return True
 
 
-prime_list = [1] * 500000
+prime_list = [1] * 1000001
 prime_list[0] = 0
 prime_list[1] = 0
 
 # 미리 50만까지의 소수를 모두 구한다.
-# 에라토스체네스의 체 사용
-for i in range(2, int(sqrt(500000))):
+# 에라토스테네스의 체 사용
+for i in range(2, int(sqrt(1000000))):
     
     if prime_list[i] == 1 and is_prime(i) == True:
 
         j = i * 2
         
         # 해당 소수의 배수는 모두 소수 목록에서 제외
-        while j < 500000:
+        while j < 1000000:
             prime_list[j] = 0
 
             j += i
 
-# 백만 개의 경우의 수에 결과 누적
-cases = [0] * 1000001
-
-print(1)
-
+# 소수 목록에 추가
+primes = [id for id, value in enumerate(prime_list) if value == True]
 
 n = int(input())
 
 for _ in range(n):
 
     number = int(input())
-    print(cases[number])
+    cnt = 0
+
+    # number까지의 소수 목록을 순회하며, 소수가 있는지 확인
+    for p in primes:
+        
+        # 소수가 입력된 짝수의 /2을 넘어가면, 중복된 경우만 나오므로 break
+        if p > number / 2:
+            break
+        
+        # 입력된 수와 소수를 뺀 숫자가 소수라면, 두 소수의 합으로 만들 수 있는 경우
+        if prime_list[number - p] == True:
+            cnt += 1
+
+    print(cnt)
+
+
 
