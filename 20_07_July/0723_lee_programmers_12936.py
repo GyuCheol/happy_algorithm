b41a74233c8a71ff92fbf250df1bd8bf32691ac9
GyuCheol <rbcjf0219@gmail.com>
2020-07-23 20:17:31 +0900

    7/23 줄서는법(lv3) 완성!@!!

--- 20_07_July/0723_lee_programmers_12936.py
+++ 20_07_July/0723_lee_programmers_12936.py
@@ -10,10 +10,9 @@
 # 다행히 64비트를 초과하지 않는다.
 # 일단 k값을 토대로 ary의 몇번째 값이 몇번째에 오는지 유추해볼 수 있다. (사전 순이므로)
 
-# 요소가 3개짜리라면, 가장 첫 요소가 먼저 오려면은 적어도 (k-1)!번째 까지는 되어야 한다.
-# 그 다음 (k-1)번째부터 2번째 요소가 가장 첫자리에 위치할 것이다.
-# 이를 이용해 (k-1)!나누고, (k-2)! 나누어.. 0이 될 때까지 반복하여 해당 번째 자리 값을 구하면 될 것이다.
-# 확실히 lv3급 훼이크가 있는 문제지만, 코드는 짧다.
+# 요소가 3개짜리라면, 가장 첫 요소가 가장 첫 자리에 있는 경우는 (k-1)!자리까지다 (즉 2가지)
+# 그 다음 자리로는 당연히 (k-2)!자리까지가 순서상으로 올 수 있고
+# 기준이 되는 k값을 감소해가며 총 n자리까지의 각 순서상 올 수 있는 값을 구하면 된다.
 
 # O(N)
 
@@ -23,6 +22,7 @@ def solution(n, k):
     ary = [x for x in range(1, n + 1)]
     answer = []
 
+    k -= 1
 
     while n > 0:
         tmp = factorial(n-1)
@@ -30,13 +30,18 @@ def solution(n, k):
 
         answer.append(ary[k // tmp])
 
-        k //= tmp
-
-        
+        ary.remove(ary[k // tmp])
+        k -= (k // tmp) * tmp
 
     
     return answer
 
     
-
+print(solution(3, 1))
+print(solution(3, 2))
+print(solution(3, 3))
+print(solution(3, 4))
 print(solution(3, 5))
+print(solution(3, 6))
+
+print(solution(10, 1))
